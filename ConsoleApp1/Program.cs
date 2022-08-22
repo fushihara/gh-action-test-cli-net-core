c1e120507ad9f86b947fc41d154d6801ab196ce8
Fushihara <1039534+fushihara@users.noreply.github.com>
2022-08-23 06:44:07 +0900

    up2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("stdout start.");
+int count = 0;
+while (true) {
+    char c1 = (char)('A' + (count%26));
+    char c2 = (char)('a' + (count % 26));
+    count += 1;
+    Thread.Sleep(50);
+    Console.Error.Write(c1.ToString());
+    Thread.Sleep(50);
+    Console.Out.Write(c2.ToString());
+    if (c1 == 'Z') {
+        Console.Error.Write("\n");
+        Console.Out.Write("\n");
+    }
+}
